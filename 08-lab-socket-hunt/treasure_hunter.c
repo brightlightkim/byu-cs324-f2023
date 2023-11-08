e1960bd7b72bf7371d7ef6709002f43529c127a0
brightlightkim <k2289@byu.edu>
2023-11-07 23:27:14 -0700

    update treasurehunt

--- 08-lab-socket-hunt/treasure_hunter.c
+++ 08-lab-socket-hunt/treasure_hunter.c
@@ -1,50 +1,366 @@
 // Replace PUT_USERID_HERE with your actual BYU CS user id, which you can find
 // by running `id -u` on a CS lab machine.
-#define USERID PUT_USERID_HERE
+#define USERID 383650543
 
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <stdio.h>
+
+#define FIRST_REQUEST_SIZE 8
+#define REQUEST_SIZE 4
+#define RESPONSE_SIZE 256
+#define TREASURE_SIZE 1024
 
 int verbose = 0;
 
 void print_bytes(unsigned char *bytes, int byteslen);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+	unsigned char treasure[TREASURE_SIZE];
+	int treasureIndex = 0;
+	char *server = argv[1];
+	char *port = argv[2];
+
+	const int userid = htonl(USERID);
+
+	unsigned char level = argv[3][0] - 48;
+	int seed = htonl(atoi(argv[4]));
+
+	unsigned char first_request[FIRST_REQUEST_SIZE];
+	memset(first_request, 0, FIRST_REQUEST_SIZE);
+	memcpy(&first_request[1], &level, 1);
+	memcpy(&first_request[2], &userid, 4);
+	memcpy(&first_request[6], &seed, 2);
+
+	struct addrinfo hints;
+	struct addrinfo *result;
+	int sfd, s;
+
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_flags = 0;
+	hints.ai_protocol = 0;
+
+	s = getaddrinfo(server, port, &hints, &result);
+	if (s != 0)
+	{
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+		exit(EXIT_FAILURE);
+	}
+
+	if (result == NULL)
+	{
+		fprintf(stderr, "Could not connect\n");
+		exit(EXIT_FAILURE);
+	}
+
+	int af;
+	struct sockaddr_in ipv4addr_remote;
+	struct sockaddr_in ipv4addr_local;
+	struct sockaddr_in6 ipv6addr_remote;
+	struct sockaddr_in6 ipv6addr_local;
+	socklen_t addr_len;
+
+	af = result->ai_family;
+	sfd = socket(af, result->ai_socktype, 0);
+
+	if (af == AF_INET)
+	{
+		ipv4addr_remote = *(struct sockaddr_in *)result->ai_addr;
+		addr_len = sizeof(struct sockaddr_in);
+		getsockname(sfd, (struct sockaddr *)&ipv4addr_local, &addr_len);
+	}
+	else
+	{
+		ipv6addr_remote = *(struct sockaddr_in6 *)result->ai_addr;
+		addr_len = sizeof(struct sockaddr_in6);
+		getsockname(sfd, (struct sockaddr *)&ipv6addr_local, &addr_len);
+	}
+
+	unsigned char response[RESPONSE_SIZE];
+	ssize_t bytesRecieved;
+	if (af == AF_INET)
+	{
+		if (sendto(sfd, first_request, FIRST_REQUEST_SIZE, 0, (struct sockaddr *)&ipv4addr_remote, addr_len) < 0)
+		{
+			perror("sendto()");
+		}
+		if ((bytesRecieved = recvfrom(sfd, response, RESPONSE_SIZE, 0, (struct sockaddr *)&ipv4addr_remote, &addr_len)) < 0)
+		{
+			perror("recvfrom()");
+		}
+	}
+	else
+	{
+		if (sendto(sfd, first_request, FIRST_REQUEST_SIZE, 0, (struct sockaddr *)&ipv6addr_remote, addr_len) < 0)
+		{
+			perror("sendto()");
+		}
+		if ((bytesRecieved = recvfrom(sfd, response, RESPONSE_SIZE, 0, (struct sockaddr *)&ipv6addr_remote, &addr_len)) < 0)
+		{
+			perror("recvfrom()");
+		}
+	}
+
+	freeaddrinfo(result);
+
+	unsigned char chunkLength = 0;
+	unsigned char chunk[RESPONSE_SIZE];
+	unsigned char opCode = 0;
+	unsigned short opParam = 0;
+	unsigned int nonce = 0;
+
+	do
+	{
+		memcpy(&chunkLength, &response[0], 1);
+		if (chunkLength == 0 || chunkLength >= 129)
+		{
+			break;
+		}
+		memcpy(&chunk, &response[1], chunkLength);
+		memcpy(&opCode, &response[chunkLength + 1], 1);
+		memcpy(&opParam, &response[chunkLength + 2], 2);
+		memcpy(&nonce, &response[chunkLength + 4], 4);
+		nonce = ntohl(nonce);
+		nonce++;
+		nonce = htonl(nonce);
+
+		switch (opCode)
+		{
+		case 0:
+			break;
+			
+		case 1:
+			if (af == AF_INET)
+			{
+				ipv4addr_remote.sin_port = opParam;
+			}
+			else
+			{
+				ipv6addr_remote.sin6_port = opParam;
+			}
+			break;
+
+		case 2:
+			close(sfd);
+			sfd = socket(af, SOCK_DGRAM, 0);
+
+			if (af == AF_INET)
+			{
+				ipv4addr_local.sin_family = AF_INET;
+				ipv4addr_local.sin_port = opParam;
+				ipv4addr_local.sin_addr.s_addr = 0;
+				if (bind(sfd, (struct sockaddr *)&ipv4addr_local, addr_len) < 0)
+				{
+					perror("bind()");
+				}
+			}
+			else
+			{
+				ipv6addr_local.sin6_family = AF_INET6;
+				ipv6addr_local.sin6_port = opParam;
+				bzero(ipv6addr_local.sin6_addr.s6_addr, 16);
+
+				if (bind(sfd, (struct sockaddr *)&ipv6addr_local, addr_len) < 0)
+				{
+					perror("bind()");
+				}
+			}
+			break;
+
+		case 3:
+			nonce = 0;
+			opParam = ntohs(opParam);
+			if (af == AF_INET)
+			{
+				struct sockaddr_in getPorts = ipv4addr_remote;
+				for (int i = 0; i < opParam; i++)
+				{
+					if ((bytesRecieved = recvfrom(sfd, response, RESPONSE_SIZE, 0, (struct sockaddr *)&getPorts, &addr_len)) < 0)
+					{
+						perror("recvfrom()");
+					}
+					nonce += ntohs(getPorts.sin_port);
+				}
+			}
+			else
+			{
+				struct sockaddr_in6 getPorts = ipv6addr_remote;
+				for (int i = 0; i < opParam; i++)
+				{
+					if ((bytesRecieved = recvfrom(sfd, response, RESPONSE_SIZE, 0, (struct sockaddr *)&getPorts, &addr_len)) < 0)
+					{
+						perror("recvfrom()");
+					}
+					nonce += ntohs(getPorts.sin6_port);
+				}
+			}
+			nonce++;
+			nonce = htonl(nonce);
+			break;
+
+		case 4:
+			if (af == AF_INET)
+			{
+				hints.ai_family = AF_INET6;
+				sprintf(port, "%d", ntohs(opParam));
+				s = getaddrinfo(server, port, &hints, &result);
+
+				if (s != 0)
+				{
+					fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+					exit(EXIT_FAILURE);
+				}
+
+				if (result == NULL)
+				{
+					fprintf(stderr, "Could not connect\n");
+					exit(EXIT_FAILURE);
+				}
+
+				af = result->ai_family;
+				close(sfd);
+				sfd = socket(af, result->ai_socktype, 0);
+				ipv6addr_remote = *(struct sockaddr_in6 *)result->ai_addr;
+				addr_len = sizeof(struct sockaddr_in6);
+				getsockname(sfd, (struct sockaddr *)&ipv6addr_local, &addr_len);
+			}
+			else
+			{
+				hints.ai_family = AF_INET;
+				sprintf(port, "%d", ntohs(opParam));
+				s = getaddrinfo(server, port, &hints, &result);
+
+				if (s != 0)
+				{
+					fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+					exit(EXIT_FAILURE);
+				}
+
+				if (result == NULL)
+				{
+					fprintf(stderr, "Could not connect\n");
+					exit(EXIT_FAILURE);
+				}
+
+				af = result->ai_family;
+				close(sfd);
+				sfd = socket(af, result->ai_socktype, 0);
+				ipv4addr_remote = *(struct sockaddr_in *)result->ai_addr;
+				addr_len = sizeof(struct sockaddr_in);
+				getsockname(sfd, (struct sockaddr *)&ipv4addr_local, &addr_len);
+			}
+
+			break;
+
+		default:
+			printf("Unknown opCode: %c\n", opCode);
+			break;
+		}
+
+		memcpy(&treasure[treasureIndex], &chunk, chunkLength);
+		treasureIndex += chunkLength;
+
+		unsigned char request[REQUEST_SIZE];
+		memcpy(&request, &nonce, 4);
+
+		if (af == AF_INET)
+		{
+			if (sendto(sfd, request, REQUEST_SIZE, 0, (struct sockaddr *)&ipv4addr_remote, addr_len) < 0)
+			{
+				perror("sendto()");
+			}
+			if ((bytesRecieved = recvfrom(sfd, response, RESPONSE_SIZE, 0, (struct sockaddr *)&ipv4addr_remote, &addr_len)) < 0)
+			{
+				perror("recvfrom()");
+			}
+		}
+		else
+		{
+			if (sendto(sfd, request, REQUEST_SIZE, 0, (struct sockaddr *)&ipv6addr_remote, addr_len) < 0)
+			{
+				perror("sendto()");
+			}
+			if ((bytesRecieved = recvfrom(sfd, response, RESPONSE_SIZE, 0, (struct sockaddr *)&ipv6addr_remote, &addr_len)) < 0)
+			{
+				perror("recvfrom()");
+			}
+		}
+	} while (chunkLength != 0);
+
+	treasure[treasureIndex] = 0;
+
+	printf("%s\n", treasure);
+
+	close(sfd);
 }
 
-void print_bytes(unsigned char *bytes, int byteslen) {
+void print_bytes(unsigned char *bytes, int byteslen)
+{
 	int i, j, byteslen_adjusted;
 
-	if (byteslen % 8) {
+	if (byteslen % 8)
+	{
 		byteslen_adjusted = ((byteslen / 8) + 1) * 8;
-	} else {
+	}
+	else
+	{
 		byteslen_adjusted = byteslen;
 	}
-	for (i = 0; i < byteslen_adjusted + 1; i++) {
-		if (!(i % 8)) {
-			if (i > 0) {
-				for (j = i - 8; j < i; j++) {
-					if (j >= byteslen_adjusted) {
+	for (i = 0; i < byteslen_adjusted + 1; i++)
+	{
+		if (!(i % 8))
+		{
+			if (i > 0)
+			{
+				for (j = i - 8; j < i; j++)
+				{
+					if (j >= byteslen_adjusted)
+					{
 						printf("  ");
-					} else if (j >= byteslen) {
+					}
+					else if (j >= byteslen)
+					{
 						printf("  ");
-					} else if (bytes[j] >= '!' && bytes[j] <= '~') {
+					}
+					else if (bytes[j] >= '!' && bytes[j] <= '~')
+					{
 						printf(" %c", bytes[j]);
-					} else {
+					}
+					else
+					{
 						printf(" .");
 					}
 				}
 			}
-			if (i < byteslen_adjusted) {
+			if (i < byteslen_adjusted)
+			{
 				printf("\n%02X: ", i);
 			}
-		} else if (!(i % 4)) {
+		}
+		else if (!(i % 4))
+		{
 			printf(" ");
 		}
-		if (i >= byteslen_adjusted) {
+		if (i >= byteslen_adjusted)
+		{
 			continue;
-		} else if (i >= byteslen) {
+		}
+		else if (i >= byteslen)
+		{
 			printf("   ");
-		} else {
+		}
+		else
+		{
 			printf("%02X ", bytes[i]);
 		}
 	}
