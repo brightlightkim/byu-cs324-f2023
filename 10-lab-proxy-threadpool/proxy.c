1d5d8aeaa189a40b4442bee396f4c90c44beb1fb
brightlightkim <k2289@byu.edu>
2023-11-21 17:00:55 -0700

    change

--- 10-lab-proxy-threadpool/proxy.c
+++ 10-lab-proxy-threadpool/proxy.c
@@ -17,22 +17,22 @@ int parse_request(char *, char *, char *, char *, char *);
 void test_parser();
 void print_bytes(unsigned char *, int);
 int open_sfd();
-void handle_client(int);
+void handle_client();
 
 int main(int argc, char *argv[])
 {
     struct sockaddr_storage peer_addr;
-    socklen_t peer_addr_len;
-    int sfd;
+	socklen_t peer_addr_len;
+	int sfd, connfd, i;
+	pthread_t tid;
 
-    printf("%s\n", user_agent_hdr);
-    sfd = open_sfd();
-    while (1)
-    {
-        peer_addr_len = sizeof(struct sockaddr_storage);
-        accept(sfd, (struct sockaddr *)&peer_addr, &peer_addr_len);
-        handle_client(sfd);
-    }
+	printf("%s\n", user_agent_hdr);
+	sfd = open_sfd(argv[1]);
+	while (1) {
+		peer_addr_len = sizeof(struct sockaddr_storage);
+		connfd = accept(sfd, (struct sockaddr *) &peer_addr, &peer_addr_len);
+        handle_client(connfd);
+	}
 
     return 0;
 }
@@ -40,155 +40,149 @@ int main(int argc, char *argv[])
 int open_sfd()
 {
     struct addrinfo hints;
-	int sfd, s;
-	struct addrinfo *result;
+    int sfd;
+    struct addrinfo *result;
 
-	memset(&hints, 0, sizeof(struct addrinfo));
+    memset(&hints, 0, sizeof(struct addrinfo));
 
-	hints.ai_family = AF_INET;	/* Choose IPv4 or IPv6 */
-	hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
-	hints.ai_flags = AI_PASSIVE;	/* For wildcard IP address */
-	hints.ai_protocol = 0;		  /* Any protocol */
-	hints.ai_canonname = NULL;
-	hints.ai_addr = NULL;
-	hints.ai_next = NULL;
+    hints.ai_family = AF_INET;       /* Choose IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
+    hints.ai_flags = AI_PASSIVE;     /* For wildcard IP address */
+    hints.ai_protocol = 0;           /* Any protocol */
+    hints.ai_canonname = NULL;
+    hints.ai_addr = NULL;
+    hints.ai_next = NULL;
 
-	if ((s = getaddrinfo(NULL, '8080', &hints, &result)) < 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
-		exit(EXIT_FAILURE);
-	}
+    if ((sfd = socket(result->ai_family, result->ai_socktype, 0)) < 0)
+    {
+        perror("Error creating socket");
+        exit(EXIT_FAILURE);
+    }
 
-	if ((sfd = socket(result->ai_family, result->ai_socktype, 0)) < 0) {
-		perror("Error creating socket");
-		exit(EXIT_FAILURE);
-	}
+    int optval = 1;
+    setsockopt(sfd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval));
 
-	int optval = 1;
-	setsockopt(sfd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval));
+    if (bind(sfd, result->ai_addr, result->ai_addrlen) < 0)
+    {
+        perror("Could not bind");
+        exit(EXIT_FAILURE);
+    }
 
-	if (bind(sfd, result->ai_addr, result->ai_addrlen) < 0) {
-		perror("Could not bind");
-		exit(EXIT_FAILURE);
-	}
+    freeaddrinfo(result); /* No longer needed */
 
-	freeaddrinfo(result);
+    if (listen(sfd, 100) < 0)
+    {
+        perror("Could not listen");
+        exit(EXIT_FAILURE);
+    }
 
-	if (listen(sfd, 100) < 0) { 
-		perror("Could not listen");
-		exit(EXIT_FAILURE);
-	}
-
-	return sfd;
+    return sfd;
 }
 
-void handle_client(int sfd)
+void handle_client()
 {
-    ssize_t nread;
-    char buf[MAX_OBJECT_SIZE];
+    // Given a newly created file descriptor, returned from accept(), handle a client HTTP request. For now, just have this method do the following:
+    // Read from the socket into a buffer until the entire HTTP request has been received. Again, there is no request body in this lab, so this is basically just the end of headers.
+    // Print out the HTTP request using print_bytes(). This will allow you to see the entire request.
+    // Add a null-terminator to the HTTP request, and pass it to the parse_request() function, allowing it to extract the individual values associated with the request.
+    // Print out the components of the HTTP request, once you have received it in its entirety (e.g., like test_parser() does). This includes the method, hostname, port, and path. Because these should all be null-terminated strings of type char [], you can use printf().
+    // Close the socket. Later will you replace printing the values with more meaningful functionality. This first part is just to get you going in the right direction.
+    ssize_t nread, nwrite;
+    char buf[MAX_OBJECT_SIZE], req[MAX_OBJECT_SIZE];
+    char method[16], hostname[64], port[8], path[64], req_headers[1024];
     int total = 0;
-    char method[16], hostname[64], port[8], path[64];
+    struct addrinfo hints;
+    struct addrinfo *result;
+    int sfd2, s;
+    memset(req, 0, MAX_OBJECT_SIZE);
     memset(buf, 0, MAX_OBJECT_SIZE);
-    while (1) {
-		nread = recv(sfd, buf+total, sizeof(buf)-total, 0);
-		total += nread;
-		if (parse_request(buf, method, hostname, port, path)) {
-			break;
-		}
-	}
-    print_bytes((unsigned char *)buf, total);
 
-    
-    // ssize_t nread, nwrite;
-	// char buf[MAX_OBJECT_SIZE], req[MAX_OBJECT_SIZE];
-	// char method[16], hostname[64], port[8], path[64], req_headers[1024];
-	// int total = 0;
-	// struct addrinfo hints;
-	// struct addrinfo *result;
-	// int sfd2, s;
-	// memset(req, 0, MAX_OBJECT_SIZE);
-	// memset(buf, 0, MAX_OBJECT_SIZE);
+    int sfd = open_sfd();
 
-	// while (1) {
-	// 	nread = recv(sfd, buf+total, sizeof(buf)-total, 0);
-	// 	total += nread;
-	// 	if (parse_request(buf, method, hostname, port, path)) {
-	// 		break;
-	// 	}
-	// }
+    while (1)
+    {
+        nread = recv(sfd2, buf + total, sizeof(buf) - total, 0);
+        total += nread;
+        if (parse_request(buf, method, hostname, port, path))
+        {
+            break;
+        }
+    }
 
-    // print_bytes((unsigned char *)buf, total);
+    print_bytes(buf, total);
 
-	// printf("Received %d bytes\n",
-	// 		total);
+    // strcat(req, method);
+    // strcat(req, " ");
+    // strcat(req, path);
+    // strcat(req, " HTTP/1.0\r\n");
+    // if (strcmp(port, "80") == 0)
+    // {
+    //     sprintf(req_headers, "Host: %s\r\n%s\r\n",
+    //             hostname, user_agent_hdr);
+    // }
+    // else
+    // {
+    //     sprintf(req_headers, "Host: %s:%s\r\n%s\r\n",
+    //             hostname, port, user_agent_hdr);
+    // }
+    // strcat(req_headers, "Connection: close\r\n");
+    // strcat(req_headers, "Proxy-Connection: close\r\n\r\n");
+    // strcat(req, req_headers);
+    // printf("%s\n", req);
 
-	// printf("METHOD: %s\n", method);
-	// printf("HOSTNAME: %s\n", hostname);
-	// printf("PORT: %s\n", port);
+    // // communicate with the HTTP server
+    // hints.ai_family = AF_INET;       /* Choose IPv4 or IPv6 */
+    // hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
+    // hints.ai_flags = AI_PASSIVE;     /* For wildcard IP address */
+    // hints.ai_protocol = 0;           /* Any protocol */
+    // hints.ai_canonname = NULL;
+    // hints.ai_addr = NULL;
+    // hints.ai_next = NULL;
 
-	// strcat(req, method);
-	// strcat(req, " ");
-	// strcat(req, path);
-	// strcat(req, " HTTP/1.0\r\n");
-	// if (strcmp(port, "80") == 0) {
-	// 	sprintf(req_headers, "Host: %s\r\n%s\r\n", 
-	// 	hostname, user_agent_hdr);
-	// } else {
-	// 	sprintf(req_headers, "Host: %s:%s\r\n%s\r\n", 
-	// 	hostname, port,user_agent_hdr);
-	// }
-	// strcat(req_headers, "Connection: close\r\n");
-	// strcat(req_headers, "Proxy-Connection: close\r\n\r\n");
-	// strcat(req, req_headers);
-	// printf("%s\n", req);
+    // if ((s = getaddrinfo(hostname, port, &hints, &result)) < 0)
+    // {
+    //     fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+    //     exit(EXIT_FAILURE);
+    // }
 
-	// // communicate with the HTTP server
-	// hints.ai_family = AF_INET;	/* Choose IPv4 or IPv6 */
-	// hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
-	// hints.ai_flags = AI_PASSIVE;	/* For wildcard IP address */
-	// hints.ai_protocol = 0;		  /* Any protocol */
-	// hints.ai_canonname = NULL;
-	// hints.ai_addr = NULL;
-	// hints.ai_next = NULL;
+    // if ((sfd2 = socket(result->ai_family, result->ai_socktype, 0)) < 0)
+    // {
+    //     perror("Error creating socket");
+    //     exit(EXIT_FAILURE);
+    // }
 
-	// if ((s = getaddrinfo(hostname, port, &hints, &result)) < 0) {
-	// 	fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
-	// 	exit(EXIT_FAILURE);
-	// }
+    // if (connect(sfd2, result->ai_addr, result->ai_addrlen) < 0)
+    // {
+    //     fprintf(stderr, "Could not connect\n");
+    //     exit(EXIT_FAILURE);
+    // }
 
-	// if ((sfd2 = socket(result->ai_family, result->ai_socktype, 0)) < 0) {
-	// 	perror("Error creating socket");
-	// 	exit(EXIT_FAILURE);
-	// }
+    // freeaddrinfo(result); /* No longer needed */
 
-	// if (connect(sfd2, result->ai_addr, result->ai_addrlen) < 0) {
-	// 	fprintf(stderr, "Could not connect\n");
-	// 	exit(EXIT_FAILURE);
-	// }			
+    // if ((nwrite = send(sfd2, req, strlen(req), 0)) != strlen(req))
+    // {
+    //     fprintf(stderr, "Error sending response\n");
+    // };
+    // printf("num bytes sent to server: %ld\n", nwrite);
 
-	// freeaddrinfo(result);   /* No longer needed */
+    // total = 0;
+    // memset(buf, 0, MAX_OBJECT_SIZE);
+    // while ((nread = recv(sfd2, buf + total, sizeof(buf) - total, 0)))
+    // {
+    //     total += nread;
+    // }
+    // printf("bytes received from the server:\n %s\n", buf);
+    // printf("num bytes recieved from server: %d\n", total);
 
+    // close(sfd2);
 
-	// if ((nwrite = send(sfd2, req, strlen(req), 0)) != strlen(req)) {
-	// 	fprintf(stderr, "Error sending response\n");
-	// };
-	// printf("num bytes sent to server: %ld\n", nwrite);
+    // if ((nwrite = send(sfd, buf, total, 0)) != total)
+    // {
+    //     fprintf(stderr, "Error sending response\n");
+    // }
+    // printf("num bytes sent to client: %ld\n", nwrite);
 
-	// total = 0;
-	// memset(buf, 0, MAX_OBJECT_SIZE);
-	// while((nread = recv(sfd2, buf+total, sizeof(buf)-total, 0))){
-	// 	total += nread;
-	// }
-	// printf("bytes received from the server:\n %s\n", buf);
-	// printf("num bytes recieved from server: %d\n", total);
-
-	// close(sfd2);
-
-	// if ((nwrite = send(sfd, buf, total, 0)) != total) {
-	// 	fprintf(stderr, "Error sending response\n");
-	// }
-	// printf("num bytes sent to client: %ld\n", nwrite);
-
-	// close(sfd);
+    // close(sfd);
 }
 
 int complete_request_received(char *request)
